7cbe2a814c6adcf46f1a46a0c852b580a89d7214
interglobalmedia <interglobalmedia@gmail.com>
2018-12-20 06:20:59 -0500

    Add header and footer and styling

--- public/drawing.js
+++ public/drawing.js
@@ -1,10 +1,11 @@
-
-
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener('DOMContentLoaded', function() {
     const mouse = {
         click: false,
         move: false,
-        pos: { x: 0, y: 0 },
+        pos: {
+            x: 0,
+            y: 0
+        },
         pos_prev: false
     };
     // get canvas element and create context
@@ -12,34 +13,43 @@ document.addEventListener("DOMContentLoaded", function () {
     const context = canvas.getContext('2d');
     const width = window.innerWidth;
     const height = window.innerHeight;
+    // resize canvas size to size of window
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    // base settings
+    canvas.style.backgroundColor = '#d2cc77';
+
     const socket = io.connect();
-    
-    // set canvas to full browser width/height
-    canvas.width = width;
-    canvas.height = height;
 
     // register mouse event handlers
-    canvas.onmousedown = function (e) { mouse.click = true; };
-    canvas.onmouseup = function (e) { mouse.click = false; };
-    canvas.onmousemove = function (e) {
+    canvas.onmousedown = function(e) {
+        mouse.click = true;
+    };
+    canvas.onmouseup = function(e) {
+        mouse.click = false;
+    };
+    canvas.onmousemove = function(e) {
         // normalize mouse position to range 0.0 - 1.0
         mouse.pos.x = e.clientX / width;
         mouse.pos.y = e.clientY / height;
         mouse.move = true;
     };
-
     // draw line received from server
-    socket.on('draw_line', function (data) {
+    socket.on('draw_line', function(data) {
+        // if (!isDrawing) return;
+        console.log(data);
         const line = data.line;
+        context.strokeStyle = 'red';
         context.beginPath();
         context.moveTo(line[0].x * width, line[0].y * height);
         context.lineTo(line[1].x * width, line[1].y * height);
+        context.lineWidth = 2;
         context.stroke();
     });
 
     socket.on('clearit', function() {
         context.clearRect(0, 0, canvas.width, canvas.height);
-        console.log("client clearit");
+        console.log('client clearit');
     });
 
     function clearit() {
@@ -57,18 +67,16 @@ document.addEventListener("DOMContentLoaded", function () {
         // check if the user is drawing
         if (mouse.click && mouse.move && mouse.pos_prev) {
             // send line to to the server
-            socket.emit('draw_line', { line: [mouse.pos, mouse.pos_prev] });
+            socket.emit('draw_line', {
+                line: [mouse.pos, mouse.pos_prev]
+            });
             mouse.move = false;
         }
-        mouse.pos_prev = { x: mouse.pos.x, y: mouse.pos.y };
+        mouse.pos_prev = {
+            x: mouse.pos.x,
+            y: mouse.pos.y
+        };
         setTimeout(mainLoop, 25);
     }
     mainLoop();
 });
-
-
-
-
-
-
-
